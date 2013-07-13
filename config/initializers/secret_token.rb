a21044bb97f96904272109bd927fee781e63f146
Joseph B La Fave <jolafave@gmail.com>
2013-07-12 21:06:41 -0700

    add config

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Otown::Application.config.secret_token = 'e705014738515d75be6441b55036ffcde1dc744bc7b9fc088cae72916a5a9c95acb620b6878bb47c69146f9c0263591cca429821558f5661c1e9f7e9a22c6037'

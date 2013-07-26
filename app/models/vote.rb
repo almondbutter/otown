d2921b43883d7c2d9a5e591ec176c307251ad5a3
Joseph B La Fave <jolafave@gmail.com>
2013-07-25 17:13:09 -0700

    fixed voter not logged in bug

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,7 +1,6 @@
 class Vote < ActiveRecord::Base
   	# attr_accessible :title, :body
   	attr_accessible :link_id, :up, :user_id
-
-  belongs_to :user
-  belongs_to :link
+    belongs_to :user
+    belongs_to :link
 end

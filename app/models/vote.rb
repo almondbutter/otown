cabe4fef22788d9f1e1f0e51dbe7d615dbc72926
Joseph B La Fave <jolafave@gmail.com>
2013-07-29 03:26:10 -0700

    added files

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,6 +1,6 @@
 class Vote < ActiveRecord::Base
   	# attr_accessible :title, :body
-  	attr_accessible :link_id, :up, :user_id
+  	attr_accessible :link_id, :up
   	validate :user_id, :uniqueness => { :scope => :link_id }
     belongs_to :user
     belongs_to :link

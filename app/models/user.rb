b6bd2cb603b44bd4b4ba7aaf79f95e837a3b0906
Joseph B La Fave <jolafave@gmail.com>
2013-08-03 01:17:45 -0700

    edited user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :username
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :user_id
 
   has_many :links
   has_many :comments    
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,7 +1,8 @@
 class Vote < ActiveRecord::Base
   	# attr_accessible :title, :body
   	attr_accessible :link_id, :up, :user_id
-  	validate :user_id, :uniqueness => { :scope => :link_id }
+  	validates :user_id, :uniqueness => { :scope => :link_id }
+
     belongs_to :user
     belongs_to :link
 end

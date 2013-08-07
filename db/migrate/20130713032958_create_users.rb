e0015b721b48c511cc544d304fe1b72d1d7d1d66
Joseph B La Fave <jolafave@gmail.com>
2013-08-07 14:03:28 -0700

    changed link

--- db/migrate/20130713032958_create_users.rb
+++ db/migrate/20130713032958_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string   :email        
-      t.string   :user_id
+      t.string   :username
       
       t.timestamps
     end

ddf53993398065e129c586e75faa70bbe79db298
Joseph B La Fave <jolafave@gmail.com>
2013-08-11 16:19:04 -0700

    fixed asset routing error

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,6 +1,6 @@
 class LinksController < ApplicationController
   def show
-  	@link = Link.find(params[:id])
+  	@link = Link.find(params[:id]) 
     @comment = Comment.new
   end
   def new
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,6 @@ class PagesController < ApplicationController
 			params[:per_page]       ||= 8
 			params[:page]           ||= 1
 
-			@links = Link.order('created_at DESC').page(params[:page].to_i).per_page(params[:per_page].to_i)
+			@links = Link.order('created_at DESC').page(params[:page].to_i).per_page(params[:per_page].to_i)	
 	end
 end
